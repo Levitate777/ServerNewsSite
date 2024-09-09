592a7cba1b86532ce8330e38a398c179f6e5dc9e
ALeventsov <a.levencov@tg.dunice.net>
2024-09-09 12:29:26 +0300

    feature/db-connect. Made minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
@@ -7,9 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  app.enableCors({ origin: true });
   await app.listen(config.get('PORT'));
 }
 bootstrap();
