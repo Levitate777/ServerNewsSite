98cfc133aeb45282cf815ef01768a4d9462683d9
ALeventsov <a.levencov@tg.dunice.net>
2024-09-18 12:20:20 +0300

    feat(user): I wrote a controller, service and module for the User entity; I made two methods to find the user and to register the user

---
+++ src/user/user.service.ts
@@ -0,0 +1,51 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import * as bcrypt from 'bcrypt';
+
+import { User } from '../models/user.model';
+import { saltOrRounds } from '../consts';
+import { TokenService } from '../token/token.service';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User)
+    private readonly userModel: typeof User,
+    private readonly tokenService: TokenService,
+  ) {}
+
+  async findOne(login: string): Promise<User | undefined> {
+    const user = await this.userModel.findOne({ where: { login: login } });
+    //console.log('service', user);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async registerUser(
+    login: string,
+    pass: string,
+    email: string,
+  ): Promise<User> {
+    const cryptPassword = await bcrypt.hash(pass, saltOrRounds);
+    const findUser = await this.userModel.findOne({
+      where: { login: login, email: email },
+    });
+    if (findUser) {
+      throw new ConflictException('User already exists');
+    }
+    const newUser = await this.userModel.create({
+      login: login,
+      password: cryptPassword,
+      email: email,
+    });
+    const { password, createdAt, updatedAt, ...user } = newUser.toJSON();
+    return user;
+  }
+}
