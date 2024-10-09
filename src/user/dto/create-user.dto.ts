e280bb673496893cf081f20c7f8ccf58749efa13
levitate777 <and.dickiy@gmail.com>
2024-10-09 16:18:35 +0300

    feat(user-dto): created a Dto to create and update user data

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,28 @@
+import { Transform } from 'class-transformer';
+import { ValidateIf, IsNotEmpty, IsEmail, IsString, Length, Matches, IsStrongPassword } from "class-validator"
+
+export class UserDto {
+	@Length(1, 255)
+	@Transform(({ value }) => value.replace(/\s+/g, ' ').trim())
+  @ValidateIf((o) => o.login !== undefined)
+  @IsNotEmpty({ message: 'Login should not be empty' })
+	login?: string
+
+  @Length(1, 255)
+  @Matches(/^\S*$/, { message: 'Email with spaces' })
+  @Transform(({ value }) => value.trim())
+  @IsString({ message: 'Interior must be a valid string' })
+  @IsEmail({}, { message: 'Invalid email format' })
+	email?: string
+
+  @Length(1, 255)
+  @IsString({ message: 'Interior must be a valid string' })
+	@IsStrongPassword({
+    minLength: 5,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1
+  })
+  password: string;
+}
---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,17 @@
+import { Transform } from 'class-transformer';
+import { ValidateIf, IsNotEmpty, IsEmail, IsString, Length, Matches } from "class-validator"
+
+export class UserDto {
+	@Length(1, 255)
+	@Transform(({ value }) => value.replace(/\s+/g, ' ').trim())
+  @ValidateIf((o) => o.login !== undefined)
+  @IsNotEmpty({ message: 'Login should not be empty' })
+	login?: string
+
+  @Length(1, 255)
+  @Matches(/^\S*$/, { message: 'Email with spaces' })
+  @Transform(({ value }) => value.trim())
+  @IsString({ message: 'Interior must be a valid string' })
+  @IsEmail({}, { message: 'Invalid email format' })
+	email?: string
+}
