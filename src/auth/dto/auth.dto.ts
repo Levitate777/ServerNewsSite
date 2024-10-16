289621d24ce48083ac7281ce4bf74360b9bc01b0
levitate777 <and.dickiy@gmail.com>
2024-10-16 21:13:59 +0300

    feat(authDto): moved functionality from createDto to authDto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,15 +1,36 @@
-import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  ValidateIf,
+  IsNotEmpty,
+  IsEmail,
+  IsString,
+  Length,
+  Matches,
+  IsStrongPassword,
+} from "class-validator"
 
 export class AuthDto {
-  @IsString()
-  login?: string;
+	@Length(1, 255)
+	@Transform(({ value }) => value.replace(/\s+/g, ' ').trim())
+  @ValidateIf((o) => o.login !== undefined)
+  @IsNotEmpty({ message: 'Login should not be empty' })
+	login?: string
 
-  @IsEmail()
-  @IsString()
-  @IsNotEmpty()
-  email: string;
+  @Length(1, 255)
+  @Matches(/^\S*$/, { message: 'Email with spaces' })
+  @Transform(({ value }) => value.trim())
+  @IsString({ message: 'Interior must be a valid string' })
+  @IsEmail({}, { message: 'Invalid email format' })
+	email?: string
 
-  @IsString()
-  @IsNotEmpty()
+  @Length(1, 255)
+  @IsString({ message: 'Interior must be a valid string' })
+	@IsStrongPassword({
+    minLength: 5,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1
+  })
   password: string;
 }
